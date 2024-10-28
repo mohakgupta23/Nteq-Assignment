1b63a717bf479e746248319dbc7fd26f1d6a3565
mohakgupta23 <mohak.gupta@nucleusteq.com>
2024-10-28 14:45:40 +0530

    Ingestion Completed

---
+++ config/sparksession.py
@@ -0,0 +1,7 @@
+import findspark
+findspark.init()
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("Assignment")\
+    .config("spark.jars", "/mnt/c/Assignment/config/spark_jars/snowflake-jdbc-3.13.30.jar,/mnt/c/Assignment/config/spark_jars/spark-snowflake_2.12-2.15.0-spark_3.4.jar")\
+    .getOrCreate()
---
+++ main.py
@@ -0,0 +1,28 @@
+import config.loggers
+import logging
+from config.sparksession import spark
+from config.snowflake import sfOptions
+import requests
+
+def data_cleaning(dataframe):
+    dataframe_without_duplicates =  dataframe.dropDuplicates()
+    cleaned_dataframe = dataframe_without_duplicates.fillna("Value not available.").select("userId", "id", "title", "body")
+    return cleaned_dataframe
+    
+def ingestion(api, table_name):
+    res = None
+    try:
+        res = requests.get(url = api)
+        status = res.status_code
+        if res != None and status == 200:
+            response = res.json()
+            dataframe = spark.createDataFrame(response)
+            cleaned_dataframe = data_cleaning(dataframe)
+            cleaned_dataframe.write.format("snowflake").options(**sfOptions).option("dbtable", f"RAW_SCHEMA.{table_name}").mode("overwrite").save()
+            logging.info("Raw data successfully stored in the database.")
+
+    except Exception as e:
+        logging.error(f"Error: {e} occured.")
+        raise
+
+    
