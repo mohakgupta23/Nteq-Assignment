f5e0c4f62396cb93fd404b8ecf473abf7988d3da
mohakgupta23 <mohak.gupta@nucleusteq.com>
2024-10-28 17:06:15 +0530

    snake_case applied

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import config.loggers
 import logging
 from config.sparksession import spark
-from config.snowflake import sfOptions
+from config.snowflake import sf_options
 import requests
 
 def data_cleaning(dataframe):
@@ -18,7 +18,7 @@ def ingestion(api, table_name):
             response = res.json()
             dataframe = spark.createDataFrame(response)
             cleaned_dataframe = data_cleaning(dataframe)
-            cleaned_dataframe.write.format("snowflake").options(**sfOptions).option("dbtable", f"RAW_SCHEMA.{table_name}").mode("overwrite").save()
+            cleaned_dataframe.write.format("snowflake").options(**sf_options).option("dbtable", f"RAW_SCHEMA.{table_name}").mode("overwrite").save()
             logging.info("Raw data successfully stored in the database.")
 
     except Exception as e:
